226e6a50f63d10e378c609bbb3b6f1bc18a7ec1b
Shivam Bhardwaj <bhardwajshivam121@gmail.com>
2025-01-09 22:28:30 -0500

    added post fields

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 from praw import Reddit
 import sys
+from utils.constants import POST_FIELDS
+
 
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
-        reddit = praw.Reddit(client_id = client_id,
+        reddit = Reddit(client_id = client_id,
                              client_secret = client_secret,
                              user_agent = user_agent)
         print("connected to reddit!!")
@@ -17,9 +19,16 @@ def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
 
 def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
     subreddit = reddit_instance.subreddit(subreddit)
-    posts = subreddit.top(time_filter, limit=limit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
 
-    print(posts)
+    post_lists = []
+
+    for post in posts:
+        post_dict = vars(post)
+        post = {key: post_dict[key] for key in POST_FIELDS}
+        post_lists.append(post)
+
+    return post_lists
 
 def transform_data(post_df: pd.DataFrame):
     post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
--- utils/constants.py
+++ utils/constants.py
@@ -17,6 +17,20 @@ INPUT_PATH = parser.get('file_paths', 'input_path')
 OUTPUT_PATH = parser.get('file_paths', 'output_path')
 
 AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
-AWS_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
+AWS_ACCESS_KEY = parser.get('aws', 'aws_access_key')
 AWS_REGION = parser.get('aws', 'aws_region')
 AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
+
+POST_FIELDS = (
+    'id',
+    'title',
+    'score',
+    'num_comments',
+    'author',
+    'created_utc',
+    'url',
+    'over_18',
+    'edited',
+    'spoiler',
+    'stickied'
+)
